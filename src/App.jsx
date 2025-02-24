293aa23817f0fde6eeefb2fb0010058f95bb09ee
Sunny Sahu <sunnysahu8210@gmail.com>
2025-02-24 21:20:47 +0530

    Initial Commit and Some implementations

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-red-300">
+        <h1>Hello</h1>
+        <h1>sunny</h1>
+        <h1>How</h1>
+        <h1>are</h1>
+        <h1>you</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
