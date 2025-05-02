580b1649012b5ed94353fefa6ead6a2dad6a0024
Sunny Sahu <sunnysahu8210@gmail.com>
2025-05-02 16:45:49 +0530

    Minor Implementation Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,57 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
   return (
     <>
-      <div className="bg-red-300">
-        <h1>Hello</h1>
-        <h1>sunny</h1>
-        <h1>How</h1>
-        <h1>are</h1>
-        <h1>you</h1>
+      <div className="flex flex-col  h-screen "> /* justify-between /*
+        <div className="w-full bg-green-400">
+          <div className=" flex justify-between items-center p-[6vw] lg:p-[2vw]">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/375px-Google_2015_logo.svg.png"
+              alt="Website Logo"
+              className="w-[40vw] h-auto sm:w-40 md:w-48 lg:w-56 xl:w-64"
+            />
+            <div className="flex gap-4 text-white bg-red-400 text-[5vw]">
+              <p>Noti</p>
+              <p>Msg</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-green-100 w-full flex items-center">
+          <div className="flex overflow-x-auto">
+            <div className="p-3 flex flex-nowrap">
+              {count.map((item) => (
+                <div
+                  key={item}
+                  className="bg-amber-400 w-[10vw] h-[10vw] rounded-full border-2 border-white flex items-center justify-center mr-2.5 shrink-0"
+                >
+                  <span className="text-[2vw] text-center truncate overflow-hidden">
+                    {"Story " + item}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="mx-1 md:mx-8">Ham</div>
+        </div>
+
+        <div className="bg-gray-300 w-full">
+          <h1>Hello</h1>
+          <h1>sunny</h1>
+          <h1>How</h1>
+          <h1>are</h1>
+          <h1>you</h1>
+        </div>
+        <div className="bg-blue-300 w-full">
+          <h1>Hello</h1>
+          <h1>sunny</h1>
+          <h1>How</h1>
+          <h1>are</h1>
+          <h1>you</h1>
+        </div>
       </div>
     </>
   );
