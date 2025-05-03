5dc0e880b218baad0ba8de9c24d59cb6220f7fed
Sunny Sahu <sunnysahu8210@gmail.com>
2025-05-03 16:49:27 +0530

    Added Footer Icons and Adjusted

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
+import { FaHome, FaSearch, FaListAlt, FaSignOutAlt } from "react-icons/fa";
 
 function FooterComponent() {
+  const footer = [
+    { label: "Home", icon: <FaHome /> },
+    { label: "Search", icon: <FaSearch /> },
+    { label: "Feed", icon: <FaListAlt /> },
+    { label: "Logout", icon: <FaSignOutAlt /> },
+  ];
+
   return (
-    <div className="bg-green-100 w-full flex items-center px-[5vw] lg:hidden ">
-      <div className='flex w-full justify-between'>
-        <div><button type="button">Home</button></div>
-        <div><button type="button">Search</button></div>
-        <div><button type="button">Feed</button></div>
-        <div><button type="button">Logout</button></div>
+    <div className="bg-gray-100 w-full lg:hidden fixed bottom-0 left-0 z-50">
+      <div className="flex justify-between items-center px-[5vw] py-2">
+        {footer.map((item, index) => (
+          <button
+            key={index}
+            type="button"
+            className="flex flex-col items-center justify-center text-gray-700 w-full text-[clamp(12px,2.5vw,16px)]"
+          >
+            <span className="text-xl">{item.icon}</span>
+            <span>{item.label}</span>
+          </button>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default FooterComponent
+export default FooterComponent;
