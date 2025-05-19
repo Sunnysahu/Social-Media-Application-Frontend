222c4c8fb3b4b348c1d7070c1104484e447fa5e4
Sunny Sahu <sunnysahu8210@gmail.com>
2025-05-19 21:21:54 +0530

    Logout Functionality

--- src/Pages/LogoutPage.jsx
+++ src/Pages/LogoutPage.jsx
@@ -1,15 +1,24 @@
-import { Link } from "react-router-dom"
-
+import { useState } from "react";
+import { useNavigate, Link} from "react-router-dom";
+import ModalComponent from "../components/ModalComponent";
 
 
 function LogoutPage() {
+
+  const [showModal, setShowModal] = useState(true);
+
+  const navigate = useNavigate()
+  const handleCloseModal = () => {
+    setShowModal(false);
+    navigate("/")
+  };
+
   return (
     <div>
-     <h1>Hi</h1>
-     <h1>Nice</h1>
-     <h1>Good</h1>
-     {console.log("Hi") 
-     }
+     {/* Show modal only when showModal is true */}
+     {showModal && (
+        <ModalComponent message="Logout Successfully" onClose={handleCloseModal} />
+      )}
 
      <Link to="/">Go Back</Link>
     </div>
