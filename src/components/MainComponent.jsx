7cdd37ea49f26f67f9724c6037d8d580a33a8f82
Sunny Sahu <sunnysahu8210@gmail.com>
2025-05-24 20:22:20 +0530

    Make the Website Responsive

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -6,15 +6,13 @@ import OnlineLayoutComponent from "./OnlineLayoutComponent";
 import PostComponent from "./PostComponent";
 function MainComponent() {
   return (
-    <div className="w-full flex flex-row">
+    <div className="w-full flex flex-row pb-[10vh]">
       <StoryLeftComponent />
       <div>
         <PostComponent images={"../../public/square.jpeg"} />
         <PostComponent images={"../../public/frontend.png"} />
-
         <PostComponent images={"../../public/vite.svg"} />
       </div>
-      {/* <StoryLayoutComponent /> */}
       <OnlineLayoutComponent />
     </div>
   );
