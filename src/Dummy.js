2b3b46160efa056f39f9595b7c4eb58ecb33bcf9
Sunny Sahu <sunnysahu8210@gmail.com>
2025-05-24 21:28:16 +0530

    Restructired Dummy Data

---
+++ src/Dummy.js
@@ -0,0 +1,49 @@
+const data = [
+  {
+    profileImage: "1",
+    name: "name1",
+    isFriend: true,
+  },
+  {
+    profileImage: "2",
+    name: "name2",
+    isFriend: true,
+  },
+  {
+    profileImage: "3",
+    name: "name3",
+    isFriend: true,
+  },
+  {
+    profileImage: "4",
+    name: "name4",
+    isFriend: true,
+  },
+  {
+    profileImage: "5",
+    name: "name5",
+    isFriend: true,
+  },
+  {
+    profileImage: "6",
+    name: "name6",
+    isFriend: true,
+  },
+  {
+    profileImage: "6",
+    name: "name6",
+    isFriend: true,
+  },
+  {
+    profileImage: "6",
+    name: "name6",
+    isFriend: true,
+  },
+  {
+    profileImage: "6",
+    name: "name6",
+    isFriend: true,
+  },
+];
+
+export { data };
